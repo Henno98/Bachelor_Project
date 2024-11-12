03afc03bd84b49d29a03f411a6a06ed4f25d52d3
Henno98 <259854@stud.inn.no>
2024-11-12 21:02:00 +0100

    map expansion :3

--- Bachelor_project/Intermediate/Build/BuildRulesProjects/Bachelor_projectModuleRules/obj/Development/Bachelor_projectModuleRules.AssemblyInfo.cs
+++ Bachelor_project/Intermediate/Build/BuildRulesProjects/Bachelor_projectModuleRules/obj/Development/Bachelor_projectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bachelor_projectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce61023a01009a9fb7192d4afc51c1afd023dda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+093a9798488d33b7d571cc854b407cc1057fb22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bachelor_projectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bachelor_projectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
