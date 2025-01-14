628547ffc86770ea44cdda9d69e36aaf97969563
Henno98 <259854@stud.inn.no>
2025-01-14 10:55:55 +0100

    updated unreal to 5.5

--- Bachelor_project/Intermediate/Build/BuildRulesProjects/Bachelor_projectModuleRules/obj/Development/Bachelor_projectModuleRules.AssemblyInfo.cs
+++ Bachelor_project/Intermediate/Build/BuildRulesProjects/Bachelor_projectModuleRules/obj/Development/Bachelor_projectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bachelor_projectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523df0a22631a4a24d78f1402616707af5ceab22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bachelor_projectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bachelor_projectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bachelor_project/Intermediate/Build/Win64/x64/Bachelor_projectEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.cpp
+++ Bachelor_project/Intermediate/Build/Win64/x64/Bachelor_projectEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.35.32215
-#include "C:/Users/jacob/Documents/GitHubMain/Bachelor_Project/Bachelor_project/Intermediate/Build/Win64/x64/Bachelor_projectEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h"
+// Compiler: 14.38.33143
+#include "SharedPCH.UnrealEd.RTTI.Cpp20.h"
