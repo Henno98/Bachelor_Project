1439ff32c4ba541db49143a1cfbf1b30786cb7f7
Niklas Lauritzen <263851@stud.inn.no>
2025-01-24 14:06:23 +0100

    Delete BIS
    Binary, inter, saved

--- Bachelor_project/Intermediate/Build/Win64/x64/Bachelor_projectEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Github/Bachelor/Bachelor_Project/Bachelor_project/Intermediate/Build/Win64/x64/Bachelor_projectEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
