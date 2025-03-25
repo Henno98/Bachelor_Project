ba385d4324fac301f1a96201956ba3e30767665c
Niklas Lauritzen <263851@stud.inn.no>
2025-03-25 10:35:20 +0100

    BirdBoss class added

--- Bachelor_project/Intermediate/Build/Win64/UnrealEditor/Inc/Bachelor_project/UHT/Bachelor_project.init.gen.cpp
+++ Bachelor_project/Intermediate/Build/Win64/UnrealEditor/Inc/Bachelor_project/UHT/Bachelor_project.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeBachelor_project_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB7E7B06E,
-				0xE3463DF4,
+				0x5A032C76,
+				0xEC45517B,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Bachelor_project.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Bachelor_project.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Bachelor_project(Z_Construct_UPackage__Script_Bachelor_project, TEXT("/Script/Bachelor_project"), Z_Registration_Info_UPackage__Script_Bachelor_project, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB7E7B06E, 0xE3463DF4));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Bachelor_project(Z_Construct_UPackage__Script_Bachelor_project, TEXT("/Script/Bachelor_project"), Z_Registration_Info_UPackage__Script_Bachelor_project, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5A032C76, 0xEC45517B));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
